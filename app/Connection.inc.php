e0118323a33c4459a662df67d56969a3113b6b83
Esteban <estebanmejia277@gmail.com>
2018-06-15 22:30:43 -0500

    database connection

--- app/Connection.inc.php
+++ app/Connection.inc.php
@@ -1,8 +1,37 @@
 <?php
-class Conexion{
-  private static $conexion;
+class Connection{
+  private static $connection;
+  public static function openConnection(){
+    if(!isset(self::$connection)){ /* if connection is started */
+      try {
+        include_once 'config.inc.php';
+        self::$connection = new PDO("mysql:host=$server;dbname=$database", $user_name, $passwd); /*open connection*/
+        self::$connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); /*show erro*/
+        self::$connection -> exec("SET CHARACTER SET utf8");
+        print 'OPEN';
 
-  public static function openConnextion(){}
+      } catch (PDOException $e) {
+        print('ERROR:'.$e->getMessage()."<br>");
+        die();
+      }
+    }
+  }
+
+  public static function closeConnection(){
+    if(isset(self::$connection)){
+      self:$connection = null;
+      print 'CLOSED';
+    }
+  }
+
+  public static function getConnection(){
+    return self::$connection;
+  }
+
+
+
+
+
 
 }
 ?>
