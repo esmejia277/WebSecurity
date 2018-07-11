2e025af1765f7620844d737bbe03dc2f4d997345
Esteban <estebanmejia277@gmail.com>
2018-07-11 01:24:24 -0500

    sql script fixed

--- database/script.sql
+++ database/script.sql
@@ -14,12 +14,12 @@ create table entries(
   id int not null unique auto_increment,
   author_id int not null,
   title varchar(255) not null unique,
-  text character set utf8 not null,
+  text text character set utf8 not null,
   date datetime not null,
   active tinyint not null,
   primary key(id),
   foreign key (author_id) references users (id)
-    on update cascade --update data
+    on update cascade
     on delete restrict
 );
 
@@ -28,15 +28,13 @@ create table comments(
   author_id int not null,
   entry_id int not null,
   title varchar(255) not null,
-  text character set utf8 not null,
+  text text character set utf8 not null,
   date datetime not null,
   primary key (id),
   foreign key (author_id) references users (id)
-    on update cascade --update data
+    on update cascade
     on delete restrict,
   foreign key (entry_id) references entries (id)
-    on update cascade --update data
+    on update cascade
     on delete restrict
 );
-
-insert into users (name, email, passw, date_sign, active) values ("Esteban", "estebanmejia277@gmail.com", "esteban123", "2015-08-13 05:06:09", 1);
