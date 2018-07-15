c84bfcf2aee06d7178d18ad0fd07597e9fa83f50
Esteban <estebanmejia277@gmail.com>
2018-07-14 23:54:57 -0500

    new routes added

--- app/Config.inc.php
+++ app/Config.inc.php
@@ -11,3 +11,8 @@ define("register", server . "/register.php");
 define("login", server . "/login.php");
 define("correct_register" , server . "/succesfull_register.php");
 define("logout", server . "/logout.php");
+
+//rosources
+define("css", server . "/css/");
+define("js", server . "/js/");
+define("icons", server . "/fontawesome-free-5.0.13/web-fonts-with-css/css/");
